817b8a6f9aeafcb93fc9e395ca7b67fff5ef376c
Lubaluckshmi <lubov.loskutova@mail.ru>
2023-11-16 15:12:10 +0300

    Added Homework3

--- src/test/java/seminar3/tdd/UserRepositoryTest.java
+++ src/test/java/seminar3/tdd/UserRepositoryTest.java
@@ -8,16 +8,17 @@ import static org.assertj.core.api.Assertions.*;
 
 class UserRepositoryTest {
     UserRepository repository;
+    User user;
 
     @BeforeEach
     void setUp() {
         repository = new UserRepository();
+        user = new User("1", "1", true);
 
     }
 
     @Test
     void addUserToRepo() {
-        User user = new User("1", "1", true);
         user.authenticate("1", "1");
         repository.addUser(user);
         assertTrue(repository.data.contains(user));
@@ -25,9 +26,26 @@ class UserRepositoryTest {
 
     @Test
     void notAddUserToRepo() {
-        User user = new User("1", "1", true);
         user.authenticate("3", "1");
         repository.addUser(user);
         assertFalse(repository.data.contains(user));
+    }
+
+
+    @Test
+    void logOutAdmin() {
+        user.authenticate("1", "1");
+        repository.addUser(user);
+        repository.logOutNotAdminUsers();
+        assertTrue(user.isAuthenticate);
+
+    }
+    @Test
+    void logOutNotAdmin() {
+        User notAdmin = new User("1","1", false);
+        user.authenticate("1", "1");
+        repository.addUser(notAdmin);
+        repository.logOutNotAdminUsers();
+        assertFalse(notAdmin.isAuthenticate);
     }
 }
